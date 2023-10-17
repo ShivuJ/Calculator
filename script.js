1470fda6b030185891e46a1c011a0519df2ef347
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2023-10-17 20:46:36 +0530

    Completed process of Calculator

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let display = document.getElementById('display');
+let display = document.getElementById("display");
 let currentValue = "";
 
 function appendChar(char) {
     currentValue += char;
     display.value = currentValue;
+}
+
+function deleteChar() {
+    currentValue = currentValue.slice(0, -1);
+    display.value = currentValue;
+}
+
+function clearChar() {
+    currentValue = "";
+    display.value = currentValue;
+}
+
+function calculate() {
+    currentValue = eval(currentValue);
+    display.value = currentValue;
+    currentValue += char;
 }
